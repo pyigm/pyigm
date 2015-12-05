e2f92ef55e5f00ddaa6e9b94707f9009846f26b1
J. Xavier Prochaska <xavier@ucolick.org>
2015-12-04 17:29:03 -0800

    lls

---
+++ pyigm/abssys/tests/test_init_igmsys.py
@@ -0,0 +1,23 @@
+# Module to run tests on generating IGMSystem
+
+from __future__ import print_function, absolute_import, division, unicode_literals
+
+# TEST_UNICODE_LITERALS
+
+import pytest
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import numpy as np
+
+from pyigm.igmsys import IGMSystem
+
+import pdb
+
+def test_init():
+    # Simple properties
+    radec = SkyCoord(ra=123.1143*u.deg, dec=-12.4321*u.deg)
+    igmsys = IGMSystem('MgII', radec, 1.244, [-500,500]*u.km/u.s, NHI=16.)
+    # Test
+    np.testing.assert_allclose(igmsys.zabs,1.244)
+
+
