4e90d003edacc59d345ea9ee4426999143b46a72
J. Xavier Prochaska <xavier@ucolick.org>
2016-01-01 10:34:08 -0800

    coshalo files

--- pyigm/cgm/setup_package.py
+++ pyigm/cgm/setup_package.py
@@ -2,4 +2,4 @@ def get_package_data():
     # Installs the testing data files. Unable to get package_data
     # to deal with a directory hierarchy of files, so just explicitly list.
     return {'xastropy.cgm.tests': ['files/*.fits', 'files/*.dat',
-                                   'files/*.all']}
+                                   'files/*.all', 'files/*.fits.gz']}
