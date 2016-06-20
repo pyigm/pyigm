a891b198846a9e43ea2d237aecbb0224cfc9f56b
J. Xavier Prochaska <xavier@ucolick.org>
2016-06-20 06:32:39 -0700

    edits to EB

--- pyigm/igm/reion.py
+++ pyigm/igm/reion.py
@@ -4,16 +4,17 @@ import pdb
 
 import numpy as np
 
-from astropy import units as u
 from astropy import constants as const
 from astropy.cosmology import Planck15
 
 from linetools.lists.linelist import LineList
 
 
-def igm_damping(wave, zend, zsource):
+def igm_damping(wave, zend, zsource, cosmo=Planck15):
     """  Calculates IGM 'damping' wing based on Miralda-Escude 1998 formalism
-    However, there were major errors with his Appendix..
+    However, there were errors with his Appendix..
+
+    Assumes a fully neutral medium at the mean density
 
     Note: This approximation is only valid for the optical depth away from
     line center
@@ -36,11 +37,11 @@ def igm_damping(wave, zend, zsource):
     # Constants
     HIlines= LineList('HI')
     Lya = HIlines['HI 1215']
-    Ralpha = (Lya['A'] / 4 / np.pi / (const.c/Lya['wrest'])).decompose()  # 2.02e-8  # A_12 / 4 pi nu_12
+    Ralpha = (Lya['A'] / 4. / np.pi / (const.c/Lya['wrest'])).decompose()  # 2.02e-8  # A_12 / 4 pi nu_12
 
     # Mean hydrogen density today
     Y = 0.25  # Helium mass fraction
-    rho_b = Planck15.Ob0 * Planck15.critical_density0.cgs
+    rho_b = cosmo.Ob0 * cosmo.critical_density0.cgs
     nH = rho_b * (1-Y) / const.m_p.cgs
 
     # Offsets
@@ -49,22 +50,30 @@ def igm_damping(wave, zend, zsource):
     delta = (dwave / wave0).decompose()
 
     # tau0 -- Note that M98 has a number of typos including the speed of light!
-    tau0 = (3 * Lya['wrest']**3 * Lya['A'] * nH / (
-        8 * np.pi * Planck15.H0) * (1+zsource)**1.5).decompose()
+    tau0 = (3 * Lya['wrest']**3 * Lya['A'] * nH * (1+zsource)**1.5 /
+            (8 * np.pi * cosmo.H0)).decompose()
 
-    # Cripes, even the Integral is wrong in M98!
+    """
     def intx(x):
         # From Wolfram
         integral = np.sqrt(x)*(10*x**4 + 18*x**3 + 42*x**2 + 210*x - 315
                            )/(35*(x-1)) - 4.5*np.log((1+np.sqrt(x))/(1-np.sqrt(x)))
         return integral
+    """
 
-    def calc_intx(x1,x2):
-        return intx(x2) - intx(x1)
+    def integral_M98(x):
+        inte = x**4.5/(1-x) + (9./7.)* x**3.5 +(9./5.)*x**2.5 + 3*x**1.5
+        inte += 9*x**0.5 - 4.5*np.log((1+x**0.5)/(1-x**0.5))
+
+        return inte
+
+    def calc_intx(x1, x2):
+        return integral_M98(x2) - integral_M98(x1)
+        #return intx(x2) - intx(x1)
 
     # Calculate x1, x2
-    x1 = (1+zend)/((1+zsource)*(1+delta))
-    x2 = 1/(1+delta)
+    x1 = (1.+zend)/((1.+zsource)*(1.+delta))
+    x2 = 1./(1+delta)
 
     tau = (tau0*Ralpha/np.pi) * (1+delta)**1.5 * calc_intx(x1,x2)
 
