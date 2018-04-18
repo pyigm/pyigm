8f5188478037ea0c23d67be5d63e47311deb7653
marijana777 <msmailag@ucsc.edu>
2018-04-18 14:53:04 -0700

    class QPQ8(CGMAbsSurvey) added

--- pyigm/cgm/qpq.py
+++ pyigm/cgm/qpq.py
@@ -6,41 +6,20 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 import numpy as np
 import pdb
 from pkg_resources import resource_filename
-from astropy.table import Table, Column
-from pyigm.cgm.cgmsurvey import CGMAbsSurvey
-import pyigm
+
+from astropy.table import Table
 from astropy.coordinates import SkyCoord
-
-
-
-### added more
-
-import os, glob
-import warnings
-import h5py
-import json, yaml
-
-from astropy.io import fits, ascii
 from astropy import units as u
 
-from linetools.spectra import io as lsio
 from linetools.spectralline import AbsLine
-from linetools.analysis import absline as ltaa
-from linetools.analysis import abskin as laak
 from linetools.isgm.abscomponent import AbsComponent
 from linetools import utils as ltu
 
-from pyigm.metallicity.pdf import MetallicityPDF, DensityPDF, GenericPDF
+from pyigm.cgm.cgmsurvey import CGMAbsSurvey
+import pyigm
 from pyigm.field.galaxy import Galaxy
 from .cgm import CGMAbsSys
 from pyigm.abssys.igmsys import IGMSystem
-
-try:
-    basestring
-except NameError:  # For Python 3
-    basestring = str
-
-
 
 try:
     basestring
@@ -65,7 +44,7 @@ def load_qpq(v):
         q8file = resource_filename('pyigm', 'data/CGM/QPQ/qpq8_all_measured.dat')
         qpqdata = Table.read(q8file, format='ascii')
 
-    if v == 8.5:  ## planning to change this somehow
+    if v == 8.5:  # additional information about QPQ8
         q8file = resource_filename('pyigm', 'data/CGM/QPQ/qpq8_pairs.fits')
         qpqdata = Table.read(q8file)
 
@@ -78,7 +57,7 @@ def load_qpq(v):
         qpqdata = Table.read(q6file)
 
     if v not in [6,7,8,8.5]:
-        print('Please choose 6, 7, or 8')
+        print('Please choose 6, 7, 8, or 8.5 for QPQ6, QPQ7, QPQ8, additional information about QPQ8')
         return
 
     return qpqdata
@@ -275,7 +254,7 @@ class QPQ6(CGMAbsSurvey):
 
             # Instantiate the IGM System
             igm_sys = IGMSystem((qpqdata['RAD_BG'][i], qpqdata['DECD_BG'][i]),
-                                qpqdata['Z_LYA'][i], [-1500, 1500.] * u.km / u.s,
+                                qpqdata['Z_FG'][i], [-1500, 1500.] * u.km / u.s,
                                 abs_type='CGM')
             igm_sys.zem = qpqdata['Z_BG'][i]
             igm_sys.NHI = qpqdata['NHI'][i]
@@ -283,13 +262,14 @@ class QPQ6(CGMAbsSurvey):
             igm_sys.flag_NHI = qpqdata['FLG_NHI'][i]
             igm_sys.s2n_lya = qpqdata['S2N_LYA'][i]
             igm_sys.flg_othick = qpqdata['FLG_OTHICK'][i]
+            igm_sys.z_lya = qpqdata['Z_LYA'][i]
 
             iname = qpqdata['QSO'][i]
             # Instantiate
             rho = qpqdata['R_PHYS'][i]*u.kpc
             cgabs = CGMAbsSys(gal, igm_sys, name=iname, rho = rho, **kwargs)
             aline = AbsLine(1215.67*u.AA, closest=True, z=igm_sys.zabs)
-            aline.attrib['EW'] = qpqdata['EWLYA'][i]* u.AA   # Observed? - check
+            aline.attrib['EW'] = qpqdata['EWLYA'][i]* u.AA   # Rest EW
             aline.attrib['sig_EW'] = qpqdata['SIG_EWLYA'][i] * u.AA
             if aline.attrib['EW'] > 3. * aline.attrib['sig_EW']:
                 aline.attrib['flag_EW'] = 1
@@ -298,7 +278,7 @@ class QPQ6(CGMAbsSurvey):
 
             aline.attrib['coord'] = igm_sys.coord
             aline.limits._wvlim = qpqdata['WVMNX'][i]*u.AA
-            dv = ltu.rel_vel(aline.limits._wvlim,aline.wrest*(1+qpqdata['Z_LYA'][i]))
+            dv = ltu.rel_vel(aline.limits._wvlim,aline.wrest*(1+qpqdata['Z_FG'][i]))
             aline.limits._vlim = dv
 
             abslines = []
@@ -356,7 +336,7 @@ class QPQ7(CGMAbsSurvey):
 
             # Instantiate the IGM System
             igm_sys = IGMSystem((qpqdata['RAD_BG'][i], qpqdata['DECD_BG'][i]),
-                                qpqdata['Z_LYA'][i], [-1500, 1500.] * u.km / u.s,
+                                qpqdata['Z_FG'][i], [-1500, 1500.] * u.km / u.s,
                                 abs_type='CGM')
             igm_sys.zem = qpqdata['Z_BG'][i]
             igm_sys.NHI = qpqdata['NHI'][i]
@@ -364,13 +344,14 @@ class QPQ7(CGMAbsSurvey):
             igm_sys.flag_NHI = qpqdata['FLG_NHI'][i]
             igm_sys.s2n_lya = qpqdata['S2N_LYA'][i]
             igm_sys.flg_othick = qpqdata['FLG_OTHICK'][i]
+            igm_sys.z_lya = qpqdata['Z_LYA'][i]
 
             iname = qpqdata['QSO'][i]
             # Instantiate
             rho = qpqdata['R_PHYS'][i]*u.kpc
             cgabs = CGMAbsSys(gal, igm_sys, name=iname, rho = rho, **kwargs)
             aline = AbsLine(1215.67*u.AA, closest=True, z=igm_sys.zabs)
-            aline.attrib['EW'] = qpqdata['EWLYA'][i]* u.AA   # Observed?? - check
+            aline.attrib['EW'] = qpqdata['EWLYA'][i]* u.AA   # Rest EW
             aline.attrib['sig_EW'] = qpqdata['SIG_EWLYA'][i] * u.AA
             if aline.attrib['EW'] > 3. * aline.attrib['sig_EW']:
                 aline.attrib['flag_EW'] = 1
@@ -378,8 +359,8 @@ class QPQ7(CGMAbsSurvey):
                 aline.attrib['flag_EW'] = 3
 
             aline.attrib['coord'] = igm_sys.coord
-            #aline.limits._wvlim = qpqdata['WVMNX'][i]*u.AA   ## no data?
-            #dv = ltu.rel_vel(aline.limits._wvlim, aline.wrest * (1 + qpqdata['Z_LYA'][i]))
+            #aline.limits._wvlim = qpqdata['WVMNX'][i]*u.AA   ##   (no data in QPQ7 file)
+            #dv = ltu.rel_vel(aline.limits._wvlim, aline.wrest * (1 + qpqdata['Z_FG'][i]))
             #aline.limits._vlim = dv
 
             abslines = []
@@ -392,7 +373,7 @@ class QPQ7(CGMAbsSurvey):
                 if qpqdata[i]['FLG_METAL_EW'][j] >0:
                     wave0 = qpqdata[i]['METAL_WREST'][j]
                     iline = AbsLine(wave0*u.AA, closest=True, z=igm_sys.zabs)
-                    iline.attrib['EW'] = qpqdata['METAL_EW'][i][j]* u.AA   # Observed? - check
+                    iline.attrib['EW'] = qpqdata['METAL_EW'][i][j]* u.AA   # Rest EW
                     iline.attrib['sig_EW'] = qpqdata['METAL_SIGEW'][i][j] * u.AA
                     iline.attrib['flag_EW'] = qpqdata['FLG_METAL_EW'][i][j]
                     iline.analy['flg_eye'] = qpqdata['FLG_METAL_EYE'][i][j]
@@ -415,5 +396,140 @@ class QPQ7(CGMAbsSurvey):
 
 
 
+class QPQ8(CGMAbsSurvey):
+    """Inherits CGM Abs Survey
+        Contains information about different components
 
+        Parameters:
+        -----------
+        cdir : str, optional
+          Path to the data
+        """
+
+    def __init__(self, cdir=None, load=True, **kwargs):
+        CGMAbsSurvey.__init__(self)
+        self.survey = 'QPQ8'
+        self.ref = 'Lau+16'
+        #
+        if cdir is None:
+            self.cdir = pyigm.__path__[0] + '/data/CGM/QPQ/'
+        else:
+            self.cdir = cdir
+        self.data_file = pyigm.__path__[0] + '/data/CGM/QPQ/'+'qpq8_pairs.fits'
+
+        # Init?
+        if load:
+            self.load_data(**kwargs)
+
+    def load_data(self, **kwargs):
+        #
+        q8file = resource_filename('pyigm', 'data/CGM/QPQ/qpq8_all_measured.dat')
+        qpqdata = Table.read(q8file,format='ascii')
+        nmax = len(qpqdata)   # max number of QSOs
+        q8filecoord = resource_filename('pyigm', 'data/CGM/QPQ/qpq8_pairs.fits')
+        qpqdatacoord = Table.read(q8filecoord)
+
+        # match names with qpqdatacoord
+        qnames = []
+        for i in range(len(qpqdatacoord)):
+            qname = qpqdatacoord['QSO'][i].strip()
+            qnames.append(qname[-10:])
+
+        for i in range(nmax):
+            # match names with qpqdatacoord
+            j = np.where(np.asarray(qnames) == qpqdata['Pair'][i])[0][0]
+            # Instantiate the galaxy
+            gal = Galaxy((qpqdatacoord['RAD'][j],qpqdatacoord['DECD'][j]), z=qpqdatacoord['Z_FG'][j])
+            gal.L_BOL = qpqdatacoord['L_BOL'][j]
+            gal.L_912 = qpqdatacoord['L_912'][j]
+            gal.G_UV = qpqdatacoord['G_UV'][j]
+            gal.zsig = qpqdatacoord['Z_FSIG'][j]*u.km/u.s
+
+            # Instantiate the IGM System
+            igm_sys = IGMSystem((qpqdatacoord['RAD_BG'][j], qpqdatacoord['DECD_BG'][j]),
+                                qpqdatacoord['Z_FG'][j], [-1500, 1500.] * u.km / u.s,
+                                abs_type='CGM')
+            # Redshifts: QSO emission redshifts
+            igm_sys.zem = qpqdatacoord['Z_BG'][j]
+            igm_sys.NHI = qpqdata['HIcol'][i]
+            igm_sys.sig_NHI = [qpqdata['HIcolhierr'][i],qpqdata['HIcolloerr'][i]]
+            igm_sys.s2n_lya = qpqdatacoord['S2N_LYA'][j]
+
+            iname = qpqdata['Pair'][i]+'_'+qpqdata['subsys'][i]
+            # Instantiate
+            rho = qpqdatacoord['R_PHYS'][j]*u.kpc
+            cgabs = CGMAbsSys(gal, igm_sys, name=iname, rho = rho, **kwargs)
+
+
+            # add metal lines
+            #if False:
+            #    for jj in range(100):
+            #        flgew = 0
+            #        if qpqdatacoord['METAL_EW'][j][jj] > 0:
+            #            flgew = 1
+            #            if qpqdatacoord['METAL_EW'][j][jj] > 3*qpqdatacoord['METAL_SIGEW'][j][jj]:
+            #                flgew = 3
+            #        if flgew > 0:
+            #            wave0 = qpqdatacoord[j]['METAL_WREST'][jj]
+            #            iline = AbsLine(wave0*u.AA, closest=True, z=igm_sys.zabs)
+            #            iline.attrib['EW'] = qpqdatacoord['METAL_EW'][j][jj]* u.AA
+            #            iline.attrib['sig_EW'] = qpqdatacoord['METAL_SIGEW'][j][jj] * u.AA
+            #            iline.attrib['flag_EW'] = flgew
+            #            iline.attrib['coord'] = igm_sys.coord
+            #            abslines = []
+            #            abslines.append(iline)
+            #            comp = AbsComponent.from_abslines(abslines, chk_vel=False)
+            #            cgabs.igm_sys.add_component(comp)
+
+            ### add metal lines
+            ### not included CII*, SiII*
+            lines = [['CII 1334'], ## ['CII* 1335'],
+                     ['CIV 1548','CIV 1550'],['NI 1134', 'NI 1199'],['NII 1083'],['NV 1238','NV 1242'],
+                     ['OI 1302'],['OVI 1037'],['MgI 2852'],['MgII 2796','MgII 2803'],['AlII 1670'],['AlIII 1854','AlIII 1862'],
+                     ['SiII 1190','SiII 1193','SiII 1304','SiII 1260','SiII 1526','SiII 1808'], ## ['SiII* 1264'],
+                     ['SiIII 1206'],
+                     ['SiIV 1393','SiIV 1402'],['FeII 1608','FeII 2344','FeII 2374','FeII 2382','FeII 2586','FeII 2600'],
+                     ['FeIII 1122']]
+
+            for icmp in range(len(lines)):
+                abslines = []
+                for ii in range(len(lines[icmp])):
+                    wave0 = float(lines[icmp][ii].split(' ')[1])
+                    ewstr = str(lines[icmp][ii].split(' ')[1]) + 'EW'
+                    ewerrstr = str(lines[icmp][ii].split(' ')[1]) + 'EWerr'
+                    if ewstr == '1808EW':
+                        ewstr = '1808E'
+                    if ewerrstr == '1122EWerr':
+                        ewerrstr = '122EWerr'
+                    if qpqdata[ewstr][i] != '/':
+                        iline = AbsLine(wave0 * u.AA, closest=True, z=igm_sys.zabs)
+                        ## EW
+                        iline.attrib['EW'] = qpqdata[ewstr][i] * u.AA  # Rest EW
+                        iline.attrib['sig_EW'] = qpqdata[ewerrstr][i] * u.AA
+                        flgew = 1
+                        if iline.attrib['EW'] < 3.*iline.attrib['sig_EW']:
+                            flgew=3
+                        iline.attrib['flag_EW'] = flgew
+                        ## column densities
+                        colstr = str(lines[icmp][ii].split(' ')[0]) + 'col'
+                        colerrstr = str(lines[icmp][ii].split(' ')[0]) + 'colerr'
+                        iline.attrib['logN'] = qpqdata[colstr][i]
+                        iline.attrib['sig_logN'] = qpqdata[colerrstr][i]
+                        ##
+                        iline.attrib['coord'] = igm_sys.coord
+                        abslines.append(iline)
+                if len(abslines) > 0:
+                    comp = AbsComponent.from_abslines(abslines, chk_vel=False)
+                    cgabs.igm_sys.add_component(comp)
+
+
+            # add ang_sep
+            qsocoord = SkyCoord(ra=qpqdatacoord['RAD'][j], dec=qpqdatacoord['DECD'][j], unit='deg')
+            bgcoord = SkyCoord(ra=qpqdatacoord['RAD_BG'][j], dec=qpqdatacoord['DECD_BG'][j], unit='deg')
+            cgabs.ang_sep = qsocoord.separation(bgcoord).to('arcsec')
+
+            cgabs.vlim = [qpqdata['v_lobound'][i],qpqdata['v_upbound'][i]]*u.km/u.s
+
+
+            self.cgm_abs.append(cgabs)
 
