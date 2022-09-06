8054f77d88a3570aced0a85c38bdb7e6be9a7248
profxj <xavier@ucolick.org>
2022-09-06 11:24:34 -0700

    yaml fixes

--- pyigm/fN/tau_eff.py
+++ pyigm/fN/tau_eff.py
@@ -185,7 +185,6 @@ def lyman_ew(ilambda, zem, fN_model, NHI_MIN=11.5, NHI_MAX=22.0,
     if EW_spline is None:
         if int(bval) == 24:
             EW_FIL = os.path.join(resource_filename('pyigm', 'data'), 'fN', 'EW_SPLINE_b24.yml')
-            import pdb; pdb.set_trace()
             with open(EW_FIL, 'r') as infile:
                 EW_spline = yaml.safe_load(infile)  # dict from mk_ew_lyman_spline
         else:
